db21eaef29f7eae0836af6545b84ce4ff9a0f607
jimin3263 <dkdldsla0000@naver.com>
2021-08-16 19:52:36 +0900

    전체 글 조회

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsService.java
@@ -1,11 +1,15 @@
 package com.jojoldu.book.springboot.domain.posts;
 
+import com.jojoldu.book.springboot.web.dto.PostsListResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,5 +36,12 @@ public class PostsService {
                 .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
     }
 }
